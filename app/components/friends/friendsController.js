fbe591f86f9fcb1d18e45086a61114035f51a2df
Gwendolyn Holley <gmfholley@gmail.com>
2016-09-25 04:09:49 -0500

    First draft of find friends factory addition

--- app/components/friends/friendsController.js
+++ app/components/friends/friendsController.js
@@ -1,5 +1,6 @@
 // Controller for the Friends page
-Main.controller('FriendsCtrl', ['friendsPromise', function (friendsPromise) {
+Main.controller('FriendsCtrl', ['friendsPromise', 'findFriendsFactory', function (friendsPromise, findFriendsFactory) {
   this.test = "Hello World!";
   this.friends = friendsPromise.data;
+  this.findFriends = findFriendsFactory.findFriends;
 }])

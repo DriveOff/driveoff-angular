9f952a2ef472a28c269d7701eee0295e2b8ac02c
Gwendolyn Holley <gmfholley@gmail.com>
2016-09-25 15:10:44 -0500

    Adds method for updating profile

--- app/components/auth/profile/profileFactory.js
+++ app/components/auth/profile/profileFactory.js
@@ -1,11 +1,23 @@
 function profileFactory(localStorageService) {
   
+  
+  
   var profile = {
     name: localStorageService.get("name"),
     email: localStorageService.get("email"),
     points: localStorageService.get("points"),
     avatar: localStorageService.get("avatar"),
     id: localStorageService.get("id")
+  }
+  
+  var putURL = "https://driveoff.herokuapp.com/users";
+  
+  //TODO - add correct XHR here for updating
+  
+  profile.updateProfile = function() {
+    
+    
+    
   }
 
   return profile;
